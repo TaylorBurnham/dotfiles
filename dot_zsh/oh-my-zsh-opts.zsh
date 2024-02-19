7ee0c97a1aa17c604963af8961654a9c0fa1f77a
Taylor Burnham <taylor@burnham.io>
2024-02-19 14:14:42 -0500

    Fixed op

--- dot_zsh/oh-my-zsh-opts.zsh
+++ dot_zsh/oh-my-zsh-opts.zsh
@@ -7,10 +7,13 @@ HISTSIZE=10000
 SAVEHIST=9000
 
 # oh-my-zsh Plugins
-plugins=(git git-lfs 1password aws \
+plugins=(git git-lfs aws \
   colorize cp debian fzf ripgrep \
   pip python pyenv virtualenv \
   zsh-syntax-highlighting zsh-completions \
   zsh-interactive-cd
 )
 
+if command -v op &> /dev/null; then
+    plugins=($plugins 1password)
+fi
