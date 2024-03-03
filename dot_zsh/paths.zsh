9713f16e329dd11e5948c31fdf9dc3c460a47e4e
Taylor Burnham <taylor@burnham.io>
2024-03-03 09:53:18 -0500

    Minor Fixes
    - Updated paths to check for existence, and included NopSCADlib path
    - Noisier logins ++

--- dot_zsh/paths.zsh
+++ dot_zsh/paths.zsh
@@ -4,7 +4,10 @@ APPEND_PATHS=(
     "/usr/local/go/bin"
     "/opt/python/3.9/bin"
     "/opt/wine-stable/bin"
+    "${HOME}/Documents/OpenSCAD/libraries/NopSCADlib/scripts"
 )
 for P in "${APPEND_PATHS[@]}"; do
-    export PATH="$PATH:$P"
+    if [ -d "$P" ]; then
+        export PATH="$PATH:$P"
+    fi
 done
